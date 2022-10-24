9a83d421224ec1db121a018aa4eab80085246c4d
MW-0317 <58039651+MW-0317@users.noreply.github.com>
2022-10-24 02:58:25 -0400

    Finished window, needs bug fixing and testing

---
+++ latexsvg.py
@@ -0,0 +1,11 @@
+# TODO: Convert incoming text into pdf -> svg file or straight into svg
+
+class Converter:
+    def __init__(self, text):
+        self.text = text
+        
+    def convertPDF(self):
+        pass
+
+    def convertSVG(self):
+        pass
